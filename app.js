3aa16e62a06768a7a736b4060d11965064596462
Rob Lovelett <rlovelett@gmail.com>
2017-11-24 19:09:20 -0600

    attempting to get bugcount working. Seems to clone same bugcounts every tester

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require("path");
-var testers = require('./routes/testers'); //routes are defined here
+//var testers = require('./routes/testers'); //routes are defined here
 var fs = require('fs');
 var glob = require('glob');
 var csvParser = require('fast-csv');
-var app = express(); //Create the Express app
+var _ = require('lodash'); // needed for deduping
+var app = express(); // using for restful api
 
 // required csv files to parse
 const csvfiles_req = {
@@ -87,16 +88,17 @@ var getDevices = (stream, db) => {
 //parse bugs.csv
 var getBugCount = (stream, db) => {
     
-    var deviceId_db;
-    var testerId_db;
     var deviceId_parsed;
     var testerId_parsed;
-    var matchedTesterId;
-    var matchedTester;
-    var matchedDevice;
     
     const testersNum = db.length; // # of testers in db; need for api    
-    const devicesNum = (db) => {
+    const devicesNum = getDevicesNum(db);
+    const testers = db.map(tester => tester.firstName + tester.lastName);
+    const countries = getCountries(db);
+    const devices = getDevices(db);
+    
+    //helper functions
+    function getDevicesNum (db) {
         for (i=0; i < db.length; i++){
             if (i+1 === db.length) {
                 // # of devices assigned each user
@@ -108,29 +110,29 @@ var getBugCount = (stream, db) => {
         };
         return counter;    
     };
-    const testers = (db) => {
-        var testers = []; // init array of testers
-        var tester;  
-        for (i=0; i < testersNum; i++) {
-            tester = db[i].firstName + db[i].lastName;
-            testers[i] = tester;
-        };
-        return testers;
-    };
-    console.log(testers);
     
-    const devices = (db) => {
+    function getCountries(db) {
+        var countries = [];
+        var country;
+        for (i=0; i < db.length; i++){
+            country = db[i].Country;
+            countries[i] = country;
+        };
+        countries = _.uniq(countries); // using lodash to dedupe
+        return countries;
+    };
+    
+    function getDevices (db) {
         var devices = []; // init array of devices
         var device;
         for (i=0; i < devicesNum; i++) { // scan Devices, put in devices array
             // db array starts @ 0; Devices props start @ 1;
             device = db[testersNum - 1].Devices[i + 1].name;
-            device.trim(); // trim any spaces
+            device = device.replace(/\s/g, '');
             devices[i] = device;
         };
         return devices;
     };
-    console.log(devices);
 
     for (i=0; i < csvfiles.length; i++) {
         if (csvfiles[i] === csvfiles_req.bugs) {
@@ -139,18 +141,14 @@ var getBugCount = (stream, db) => {
             csvParser
             .fromStream(stream, {headers: [,"deviceId","testerId"]})
             .on("data", (data) => {
-                testerId_parsed = data.testerId;
-                deviceId_parsed = data.deviceId;
-                for (i=0; i < db.length; i++) {
-                    testerId_db = db[i].testerId;
-                    if (testerId_db === testerId_parsed) {
-                        // testerId_db is string, conv. to # 
-                        // find proper num tester obj. in db array 
-                        // - 1 since array starts 0
-                        matchedTester = db[Number(testerId_db) - 1];
-                        // find parsed deviceId in Tester obj. add to bugCount
-                        matchedDevice = matchedTester.Devices[deviceId_parsed];
-                        matchedDevice.bugCount ++;
+                
+                testerId_parsed = data.testerId; // make sure parsed as #
+                deviceId_parsed = Number(data.deviceId); // make sure parsed as #
+                
+                // find  testerId_parsed in db
+                for (d=0; d < db.length; d++) {
+                    if (testerId_parsed === db[d].testerId) {
+                        db[d].Devices[deviceId_parsed].bugCount ++; // add to device's bug count
                     };
                 };
             })
@@ -158,48 +156,47 @@ var getBugCount = (stream, db) => {
                 if (i+1 === csvfiles.length) {
                     throw new Error('Cannot find bugs.csv. Required to create database.');        
                 };
-
-                console.log('Done. \nDatabase created.');
-
-                // app setup
-                app.use(bodyParser.json({ extended: true }));
-                app.use(bodyParser.urlencoded({ extended: true }));
-                app.use(bodyParser.text({ extended: true }));
-                app.use(bodyParser.json({ type: "application/vnd.api+json", extended: true }));
+          
+                console.log(db[0].Devices);
+                console.log(db[1].Devices);
+                console.log(db[2].Devices);
+                console.log(db[3].Devices);
+                console.log(db[4].Devices);
+                console.log(db[5].Devices);
+                console.log(db[6].Devices);
+                console.log(db[7].Devices);
+                console.log(db[8].Devices);
                 
-                // serve static files (public/*; css,js,etc)
-                app.use(express.static(__dirname + '/public')); 
-                app.set('port', process.env.PORT || 8000); //set port
+                // routes setup
+                app.get('/', (req, res) => {
+                    console.log('get http://localhost:8000');
+                    res.sendFile(path.join(__dirname, "index.html")); // serve index.html
+                })
 
-                var server = app.listen(app.get('port'), function() {
-                    console.log('App listening on port ' + server.address().port);
+                app.get('/api', (req, res) => {
+                    res.json({message: 'This is the Testers API. Example: "/api/countries/country01&country02/devices/device01&device02"'});
                 });
+                
+                app.get('/api/countries/:countries_req/devices/:devices_req', (req, res) => {
+                    //res.json(req.params);
 
-                // routes
-                app.get("/", function(req, res) { // Basic route that sends the user first to the AJAX Page
-                    console.log('request: /n' + req);
-                    res.sendFile(path.join(__dirname, "index.html"));
-                });
-
-                // Find most experienced Tester
-                app.get('/api/countries=:country?/devices=:device?', (req, res) => {
+                    var testers_res = db; // clone init. db into testers response obj                     
+                    var countries_req = req.params.countries_req; // requested countries in api
+                    var devices_req = req.params.devices_req; // requested devices in api
+                
+                    /*
+                    console.log('Done. Database created.');
+                    console.log('testersNum: ' + testersNum);
+                    console.log('devicesNum: ' + devicesNum);
+                    console.log('testers: ' + testers);
+                    console.log('devices: ' + devices);
+                    */
                     
-                    // db
-                    // testersNum
-                    // devicesNum
-                    // db_res
-                    
-                    var testers = {
-                        "countries": req.params.country,
-                        "devices": req.params.device
-                    };
-
                     for (i=0; i < testersNum; i++) {
-                        if (testers.countries && testers.devices) {
-                            if (testers.countries === 'all' && testers.devices === 'all') {
-                                res.json('all/all');        
+                        if (countries_req && devices_req) {
+                            if (countries_req === 'all' && devices_req === 'all') {
+                                res.json(testers_res);        
                             } else if (testers.countries === 'all' && testers.devices != 'all') {
-                                // if device = 
                                 res.json('all/*');
                             } else if (testers.countries != 'all' && testers.devices === 'all') {
                                 res.json('*/all');
@@ -207,40 +204,76 @@ var getBugCount = (stream, db) => {
                         } else {
                             res.json(false);
                         };
-
-
-
-                        res.json(characters[i]);
+                        //res.json(characters[i]);
                         return;
                     };
                 });
 
-                // Search for Specific Character (or all characters) - provides JSON
+                // app setup
                 /*
-                app.get("/api/:characters?", function(req, res) {
-                    var chosen = req.params.characters;
-                
-                    if (chosen) {
-                        console.log(chosen);
-                
-                        for (var i = 0; i < characters.length; i++) {
-                            if (chosen === characters[i].routeName) {
-                            res.json(characters[i]);
-                            return;
-                            }
-                        }
-                
-                    res.json(false);
-                    }
-                    else {
-                    res.json(characters);
-                    }
-                });
+                app.use(bodyParser.json({ extended: true }));
+                app.use(bodyParser.urlencoded({ extended: true }));
+                app.use(bodyParser.text({ extended: true }));
+                app.use(bodyParser.json({ type: "application/vnd.api+json", extended: true }));
                 */
 
-                module.exports = app;
+                // serve static files (public/*; css,js,etc)
+                app.use(express.static(__dirname + '/public')); 
+                app.set('port', process.env.PORT || 8000); //set port
+
+                var server = app.listen(app.get('port'), function() {
+                    console.log('App listening on port ' + server.address().port);
+                });                
                 
+                module.exports = app;
+
             });        
         };
     };
 };
+
+// Find most experienced Tester
+/*
+app.get('/api/countries/:countries_req/devices/:device_req'), (req, res) => {
+    
+    // db
+    // testersNum
+    // devicesNum
+    // db_res
+    
+    var testers_res = db; // clone init. db into testers response obj                     
+    var countries_req = req.params.keyName.countries_req; // requested countries in api
+    var devices_req = req.params.keyName.devices_req; // requested devices in api
+
+    for (i=0; i < testersNum; i++) {
+        if (countries_req && devices_req) {
+            if (countries_req === 'all' && devices_req === 'all') {
+                res.json('all/all');        
+            } else if (testers.countries === 'all' && testers.devices != 'all') {
+                /*
+                console.log('Done. Database created.');
+                console.log('testersNum: ' + testersNum);
+                console.log('devicesNum: ' + devicesNum);
+                console.log('testers: ' + testers);
+                console.log('devices: ' + devices);
+                */
+                
+                //This route path will match butterfly and dragonfly, 
+                //but not butterflyman, dragonflyman, and so on.
+                /*
+                app.get(/.*fly$/, function (req, res) {
+                    res.send('/.*fly$/')
+                })
+                
+                
+                res.json('all/*');
+            } else if (testers.countries != 'all' && testers.devices === 'all') {
+                res.json('*//*all');
+            }
+        } else {
+            res.json(false);
+        };
+        res.json(characters[i]);
+        return;
+    };
+};*/
